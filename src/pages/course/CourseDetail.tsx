d5cac59b59d1f246cf6f18338cf78a8f4d95e9b9
juanfe <lassojuanfe@gmail.com>
2025-06-26 16:33:46 -0500

    Ajuste de typescript

--- src/pages/course/CourseDetail.tsx
+++ src/pages/course/CourseDetail.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import {
   AppShell,
