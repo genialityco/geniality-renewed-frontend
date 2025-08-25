9c8b0f81e5320126ac040a11d11b1baa84d000e4
juanfe <lassojuanfe@gmail.com>
2025-08-25 12:05:02 -0500

    Modifica funcion para cargar imagenes

--- src/utils/uploadImageToFirebase.ts
+++ src/utils/uploadImageToFirebase.ts
@@ -3,7 +3,14 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from "../firebase/firebaseConfig";
 
 export async function uploadImageToFirebase(file: File, folder = "event_images") {
-  const fileRef = ref(storage, `${folder}/${Date.now()}_${file.name}`);
-  const snap = await uploadBytes(fileRef, file, { contentType: file.type });
-  return await getDownloadURL(snap.ref);
+  const path = `${folder}/${Date.now()}_${file.name}`;
+  try {
+    const fileRef = ref(storage, path);
+    const snap = await uploadBytes(fileRef, file, { contentType: file.type });
+    const url = await getDownloadURL(snap.ref);
+    return url;
+  } catch (err: any) {
+    console.error("Storage upload error:", err?.code, err?.message);
+    throw err;
+  }
 }
